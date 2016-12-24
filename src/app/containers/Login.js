a6e304a009a019dfd0ec43888517ed263f792dce
ga21ru21 <quangtoan123@yahoo.com>
2016-12-24 17:45:30 +0700

    Refactor code login and register

--- src/app/containers/Login.js
+++ src/app/containers/Login.js
@@ -1,6 +1,94 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { loginAction } from '../actions/LoginAction';
-import LoginComponents from '../components/LoginComponent';
+import { loginAction, USER_STATUS } from '../actions/LoginAction';
+import { Link } from 'react-router';
+import { Button, Form, Input, Checkbox, Icon } from 'antd';
+import FontAwesome from 'react-fontawesome';
+import '../../css/Login.css';
+const FormItem = Form.Item;
+
+const NormalLoginForm = Form.create()(React.createClass({
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+                this.props.onSubmit(values.username, values.password);
+            }
+        });
+    },
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <Form onSubmit={this.handleSubmit} className="Login-form">
+              <FormItem>
+                  {getFieldDecorator('username', {
+                      rules: [{ required: true, message: 'Please input your username!' }],
+                  })(
+                      <Input addonBefore={<Icon type="user" />} placeholder="Username" />
+                  )}
+              </FormItem>
+              <FormItem>
+                  {getFieldDecorator('password', {
+                      rules: [{ required: true, message: 'Please input your Password!' }],
+                  })(
+                      <Input addonBefore={<Icon type="lock" />} type="password" placeholder="Password" />
+                  )}
+              </FormItem>
+              <FormItem>
+                <div>
+                    {getFieldDecorator('remember', {
+                        valuePropName: 'checked',
+                        initialValue: true,
+                    })(
+                        <Checkbox className="Login-rememberMe">Remember me</Checkbox>
+                    )}
+                  <a className="Login-form-forgot">Forgot password</a>
+                </div>
+                <div>
+                  <Button type="primary" htmlType="submit" className="Login-form-button">
+                    Log in
+                  </Button>
+                </div>
+                <div>
+                  Or <Link to="/register">register now!</Link>
+                </div>
+              </FormItem>
+            </Form>
+        );
+    }
+}));
+
+
+class LoginContainer extends Component {
+    componentWillMount() {
+        let { userStatus, router } = this.props;
+        if( userStatus === USER_STATUS.AUTHENTICATED )
+            return router.push('/');
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        let { userStatus, router } = nextProps;
+        if( userStatus === USER_STATUS.AUTHENTICATED )
+            return router.push('/');
+    }
+
+    onSubmit = (username, password) => {
+        this.props.dispatch(loginAction(username, password));
+    }
+
+    render() {
+        return (
+            <div className="Login">
+              <div className="Login-modal">
+                <FontAwesome name='user-circle-o' size='4x' className="Login-logo" inverse={true}/>
+                <div className="markdown"><h1>Login</h1></div>
+                <NormalLoginForm onSubmit={this.onSubmit}/>
+              </div>
+            </div>
+        );
+    }
+}
 
 const mapStateToProps = (state, ownProps) => {
   return {
@@ -8,17 +96,8 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-    onSubmit: (username, password) => {
-      dispatch(loginAction(username, password));
-    }
-  }
-}
-
 const Login = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(LoginComponents);
+  mapStateToProps
+)(LoginContainer);
 
 export default Login;
--- src/app/containers/Register.js
+++ src/app/containers/Register.js
@@ -1,6 +1,133 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { registerAction } from '../actions/RegisterAction';
-import RegisterComponent from '../components/RegisterComponent';
+import { Button, Form, Input, Checkbox, Icon } from 'antd';
+import FontAwesome from 'react-fontawesome';
+import '../../css/Register.css';
+const FormItem = Form.Item;
+
+const NormalRegisterForm = Form.create()(React.createClass({
+    getInitialState() {
+        return {
+            passwordDirty: false,
+        };
+    },
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    },
+    handlePasswordBlur(e) {
+        const value = e.target.value;
+        this.setState({ passwordDirty: this.state.passwordDirty || !!value });
+    },
+    checkPassowrd(rule, value, callback) {
+        const form = this.props.form;
+        if (value && value !== form.getFieldValue('password')) {
+            callback('Confirm passwords is wrong!');
+        } else {
+            callback();
+        }
+    },
+    checkConfirm(rule, value, callback) {
+        const form = this.props.form;
+        if (value && this.state.passwordDirty) {
+            form.validateFields(['confirm'], { force: true });
+        }
+        callback();
+    },
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <Form horizontal onSubmit={this.handleSubmit} className="Register-form">
+              <FormItem hasFeedback>
+                  {getFieldDecorator('username', {
+                      rules: [{
+                          required: true, message: 'Please input your username!',
+                      }],
+                  })(
+                      <Input addonBefore={<Icon type="user" />} placeholder="Username" />
+                  )}
+              </FormItem>
+              <FormItem hasFeedback>
+                  {getFieldDecorator('password', {
+                      rules: [{
+                          required: true, message: 'Please input your password!',
+                      }, {
+                          validator: this.checkConfirm,
+                      }],
+                  })(
+                      <Input addonBefore={<Icon type="lock" />} type="password" placeholder="Password" onBlur={this.handlePasswordBlur} />
+                  )}
+              </FormItem>
+              <FormItem hasFeedback>
+                  {getFieldDecorator('confirm', {
+                      rules: [{
+                          required: true, message: 'Please confirm your password!',
+                      }, {
+                          validator: this.checkPassowrd,
+                      }],
+                  })(
+                      <Input addonBefore={<Icon type="unlock" />} type="password" placeholder="Confirm Password" />
+                  )}
+              </FormItem>
+              <FormItem hasFeedback>
+                  {getFieldDecorator('email', {
+                      rules: [{
+                          type: 'email', message: 'The input is not valid E-mail!',
+                      }, {
+                          required: true, message: 'Please input your E-mail!',
+                      }],
+                  })(
+                      <Input addonBefore={<Icon type="mail" />} placeholder="Email"/>
+                  )}
+              </FormItem>
+              <FormItem className="Register-agrement">
+                  {getFieldDecorator('agreement', {
+                      valuePropName: 'checked',
+                      rules: [{
+                          required: true, message: 'Please agree with our agreement!',
+                      }],
+                  })(
+                      <Checkbox>I had read the <a>agreement</a></Checkbox>
+                  )}
+              </FormItem>
+              <FormItem>
+                <Button type="primary" htmlType="submit" size="large" className="Register-form-button">Register</Button>
+              </FormItem>
+            </Form>
+        );
+    },
+}));
+
+class RegisterContainer extends Component {
+    componentWillMount() {
+
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+
+    }
+
+    onSubmit = (username, password) => {
+        this.props.dispatch(registerAction(username, password));
+    }
+
+    render() {
+        return (
+            <div className="Register">
+              <div className="Register-modal">
+                <FontAwesome name='user-circle-o' size='4x' className="Register-logo" inverse={true}/>
+                <div className="markdown"><h1>Register</h1></div>
+                <NormalRegisterForm onSubmit={this.onSubmit}/>
+              </div>
+            </div>
+        );
+    }
+}
 
 const mapStateToProps = (state, ownProps) => {
   return {
@@ -8,17 +135,8 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-    onSubmit: (username, password) => {
-      dispatch(registerAction(username, password));
-    }
-  }
-}
+const Register = connect(
+  mapStateToProps
+)(RegisterContainer);
 
-const RegisterContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(RegisterComponent);
-
-export default RegisterContainer;
+export default Register;
